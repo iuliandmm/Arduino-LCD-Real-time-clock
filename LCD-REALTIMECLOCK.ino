d63d813605c7204bed150293708e5f9dd48250a2
iuliandmm <dumitruiulian296@gmail.com>
2025-03-18 15:48:25 +0200

    Add files via upload

---
+++ LCD-REALTIMECLOCK.ino
@@ -0,0 +1,78 @@
+#include <Wire.h>
+#include <RTClib.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// Creăm obiectul RTC
+RTC_DS3231 rtc;
+
+// Creăm obiectul LCD Nokia 5110 cu pinii corecti (4, 5, 6, 7, 8)
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(4, 5, 6, 7, 8);  // CLK, DIN, DC, CE, RST
+
+void setup() {
+  // Începem comunicarea serială pentru debug
+  Serial.begin(115200);
+
+  // Inițializăm librăria I2C
+  Wire.begin();
+
+  // Inițializăm LCD-ul Nokia 5110
+  lcd.begin();
+  lcd.setContrast(50);  // Ajustează contrastul, poți schimba valoarea pentru claritate
+  lcd.clearDisplay();
+  lcd.display();
+
+  // Verificăm dacă RTC-ul este conectat corect
+  if (!rtc.begin()) {
+    Serial.println("Nu am găsit RTC");
+    lcd.setCursor(0, 0);
+    lcd.print("Eroare RTC!");
+    lcd.display();
+    while (1); // Oprim programul dacă nu este găsit RTC-ul
+  }
+
+  // Setăm RTC-ul la ora de compilare (opțional)
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  Serial.println("RTC setat la timpul de compilare.");
+}
+
+void loop() {
+  // Obținem data și ora curentă de la RTC
+  DateTime now = rtc.now();
+
+  // Ștergem ecranul înainte de fiecare actualizare
+  lcd.clearDisplay();
+
+  // Afișăm data pe primul rând
+  lcd.setCursor(0, 0);
+  lcd.print("Data: ");
+  lcd.setCursor(0, 10);
+  lcd.print(now.day(), DEC);
+  lcd.print("-");
+  lcd.print(now.month(), DEC);
+  lcd.print("-");
+  lcd.print(now.year(), DEC);
+
+  // Lăsăm mai mult spațiu între data și ora (triplăm spațiul)
+  lcd.setCursor(0, 5);  // Mutăm cursorul pentru a lăsa 3 linii goale între data și ora
+  lcd.print("             "); // Linie goală
+
+  // Afișăm ora pe al patrulea rând
+  lcd.setCursor(0,20);  // Afișăm ora pe rândul 6
+  lcd.print("Ora: ");
+  lcd.print(now.hour(), DEC);
+  lcd.print(":");
+  lcd.print(now.minute(), DEC);
+  lcd.print(":");
+  lcd.print(now.second(), DEC);
+
+  lcd.setCursor(0,40);
+  lcd.print("DUMITRU IULIAN");
+
+  // Actualizăm display-ul
+  lcd.display();
+
+  delay(1000);  // Așteptăm 1 secundă pentru a actualiza
+}
+
+
